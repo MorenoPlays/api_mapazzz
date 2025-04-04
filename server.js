0608af3872c7d4917b2300ec144a7a6ccb03d9fd
mp <morenoplays666@gmail.com>
2025-04-04 15:19:22 +0100

    atual

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ app.use('/', publicRouter);
 app.use('/', auth, privateRouter);
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("servidor ativo");
+  console.log(port);
 }
 );
 
